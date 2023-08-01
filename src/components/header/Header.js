0d49092f3769de8e06210ad693f9c43dce9f790d
Ayana2702 <ayanashairbekkyzy@gmail.com>
2023-08-01 21:29:50 +0600

    .

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+    const navLinks =[
+        {
+            link:"ayanashairbekkyzy@gmail.com",
+
+        }
+    ]
+  return (
+    <div>Header</div>
+  )
+}
